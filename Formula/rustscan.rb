1719684213c9d5c4dcfbc4ddff6287fa3e6478ef
Brandon <brandonskerritt51@gmail.com>
2020-07-19 14:30:23 +0100

    first release

---
+++ Formula/rustscan.rb
@@ -0,0 +1,16 @@
+
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Rustscan < Formula
+
+  desc "Faster Nmap Scanning with Rust" 
+  homepage "https://github.com/brandonskerritt/rustscan"
+  url "https://github.com/brandonskerritt/RustScan/releases/download/1.0.1/rustscan.tar.gz"
+  sha256 "c8ff94630f5a7d719545287d36422698bfecca73e80f2d8219df64e0ac5b87fa"
+  version "1.0.1"
+
+  def install
+    bin.install "rustscan"
+  end
+end
