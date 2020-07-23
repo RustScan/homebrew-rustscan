01bcee92a5da1c8bf85dcdd0e2b56806dfbb1cef
Brandon <brandonskerritt51@gmail.com>
2020-07-23 11:20:08 +0100

    please work homebrew

--- Formula/rustscan.rb
+++ Formula/rustscan.rb
@@ -9,8 +9,10 @@ class Rustscan < Formula
   url "https://github.com/brandonskerritt/RustScan/releases/download/1.0.1/rustscan.tar.gz"
   sha256 "c8ff94630f5a7d719545287d36422698bfecca73e80f2d8219df64e0ac5b87fa"
   version "1.0.1"
+  depends_on "rustup" => :build
 
   def install
-    bin.install "rustscan"
+    system "rustup", "toolchain", "install", "stable"
+    system "cargo", "install", "rustscan"
   end
 end
