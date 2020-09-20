f18f7bcb6e14f4c849a06b60e2ea8d32e4592193
Brandon <brandonskerritt51@gmail.com>
2020-09-20 12:34:59 +0100

    1.9

--- Formula/rustscan.rb
+++ Formula/rustscan.rb
@@ -6,8 +6,8 @@ class Rustscan < Formula
 
   desc "Faster Nmap Scanning with Rust" 
   homepage "https://github.com/brandonskerritt/rustscan"
-  url "https://github.com/RustScan/RustScan/archive/1.8.0.tar.gz"
-  sha256 "050fb291563bcd4e3578e9d26459b044c152bde6622abf4294b55f9341b43342"
+  url "https://github.com/RustScan/RustScan/archive/1.9.0.tar.gz"
+  sha256 "9923b91310be342f5fcec42796662b53f4d0b05ec5339c44b801959ebfe824dc"
   version "1.7.1"
   depends_on "rust" => :build
 
